4bf7ad4de85a0020a17aac76bebd6c89dfb9c827
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2019-10-23 19:48:04 +0200

    BlackJack
    Jag bestämde mig för att skapa kortspelet BlackJack.

---
+++ Novemberprojekt/Novemberprojekt/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novemberprojekt
+{
+    class Card
+    {
+        private int AllCards = 52;
+        private int ColoredCards = 12;
+        private int AceCards = 4;
+        private int NormalCards;
+
+
+
+    }
+}
